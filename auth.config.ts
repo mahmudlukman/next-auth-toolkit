c3e556e6dba96f3cf1da743ad7ccc41aaf18d01d
Mahmud Lukman <mistamann49@gmail.com>
2024-01-15 15:24:34 +0100

    OAuth login setup complete

--- auth.config.ts
+++ auth.config.ts
@@ -12,7 +12,7 @@ export default {
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRETE,
-    }),,
+    }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRETE,
