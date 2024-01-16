e6dd932fbdbb51983358137171452ad9d1c3e935
Mahmud Lukman <mistamann49@gmail.com>
2024-01-16 16:12:38 +0100

    resend email verification setup complete

--- actions/login.ts
+++ actions/login.ts
@@ -7,6 +7,7 @@ import { signIn } from '@/auth';
 import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 import { getUserByEmail } from '@/data/user';
 import { generateVerificationToken } from '@/lib/tokens';
+import { sendVerificationEmail } from '@/lib/mail';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -17,15 +18,23 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 
   const { email, password } = validatedFields.data;
 
-  const existingUser = await getUserByEmail(email)
+  const existingUser = await getUserByEmail(email);
 
-  if(!existingUser || !existingUser.email || !existingUser.password) {
-    return {error: "Email does not exist!"}
+  if (!existingUser || !existingUser.email || !existingUser.password) {
+    return { error: 'Email does not exist!' };
   }
 
   if (!existingUser.emailVerified) {
-    const verificationToken = await generateVerificationToken(existingUser.email)
-    return { success: "Confirmation email sent!"}
+    const verificationToken = await generateVerificationToken(
+      existingUser.email
+    );
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
+    return { success: 'Confirmation email sent!' };
   }
 
   try {
@@ -37,10 +46,10 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
-        case "CredentialsSignin":
-          return {error: "Invalid credentials"}
-        default: 
-          return {error: "Something went wrong"}
+        case 'CredentialsSignin':
+          return { error: 'Invalid credentials' };
+        default:
+          return { error: 'Something went wrong' };
       }
     }
     throw error;
--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,7 @@ import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 import { getUserByEmail } from '@/data/user';
 import { generateVerificationToken } from '@/lib/tokens';
+import { sendVerificationEmail } from '@/lib/mail';
 
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -33,7 +34,10 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   });
 
   const verificationToken = await generateVerificationToken(email);
-  // TODO: Send verification token email
+  await sendVerificationEmail(
+    verificationToken.email,
+    verificationToken.token
+  )
 
   return { success: 'Confirm email sent!' };
 };
