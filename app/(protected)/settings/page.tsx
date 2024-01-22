2f30ce19a92e8230b835d17004bfb4e298e407b3
Mahmud Lukman <mistamann49@gmail.com>
2024-01-22 11:14:27 +0100

    settings page modified

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,22 +1,22 @@
-import { auth, signOut } from "@/auth"
+'use client';
 
-const SettingsPage = async () => {
-  const session = await auth()
+import { logout } from '@/actions/logout';
+import { useCurrentUser } from '@/hooks/use-current-user';
+
+const SettingsPage = () => {
+  const user = useCurrentUser();
+
+  const onClick = () => {
+    logout();
+  };
 
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form action={async () => {
-        "use server";
-
-        await signOut()
-      }}>
-        <button type="submit">
-          Sign Out
-        </button>
-      </form>
+    <div className='bg-white p-10 rounded-xl'>
+      <button type="submit" onClick={onClick}>
+        Sign Out
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default SettingsPage
+export default SettingsPage;
